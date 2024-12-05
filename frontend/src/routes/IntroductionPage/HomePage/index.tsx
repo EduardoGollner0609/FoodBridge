e445c84a6daf8b0b4a89d6dd56906c97f507439e
EduGollner0609 <dudugollner@gmail.com>
2024-12-05 11:07:12 -0300

    Criando seção de maiores detalhes.

--- frontend/src/routes/IntroductionPage/HomePage/index.tsx
+++ frontend/src/routes/IntroductionPage/HomePage/index.tsx
@@ -1,9 +1,13 @@
 import './styles.css';
 import imgCriancaCarente from '../../../assets/crianca-carente.jpg';
-import arrowIcon from '../../../assets/arrow.svg';
+import arrowIconPurple from '../../../assets/arrow-purple.svg';
+import arrowIconWhite from '../../../assets/arrow-white.svg';
 import donationFood from '../../../assets/donation-food.svg';
 import handsHelp from '../../../assets/hands-help.svg';
 import shareHelp from '../../../assets/share-help.svg';
+import moreDetails1 from '../../../assets/more-details-img1.png';
+import moreDetails2 from '../../../assets/more-details-img2.jpeg';
+import moreDetails3 from '../../../assets/more-details-img3.jpg';
 
 export default function HomePage() {
     return (
@@ -12,7 +16,7 @@ export default function HomePage() {
                 <div className="home-page-introduction container">
                     <div className="home-page-introduction-invite">
                         <h2>A <span>fome</span> não espera, e a <span>solidariedade</span> também não.</h2>
-                        <a href="">Doe agora <img src={arrowIcon} alt="" /></a>
+                        <a href="">Doe agora <img src={arrowIconPurple} alt="" /></a>
                     </div>
 
                     <div className="home-page-introduction-invite-img">
@@ -63,6 +67,34 @@ export default function HomePage() {
                                 <a href="">Saiba Mais</a>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </section>
+            <section id="section-home-page-more-details">
+                <div className="home-page-more-details-content container">
+                    <div className="home-page-more-details-imgs">
+                        <div className="home-page-more-details-imgs-top">
+                            <img src={moreDetails1} alt="" />
+                        </div>
+                        <div className="home-page-more-details-imgs-bottom">
+                            <img src={moreDetails2} alt="" />
+                            <img src={moreDetails3} alt="" />
+                        </div>
+
+                    </div>
+
+                    <div className="home-page-more-details-descriptions">
+                        <h2>Cada <span>Doação</span> é um passo mais perto de um mundo sem <span>Fome</span>.</h2>
+                        <p>A fome é um problema global que afeta milhões de pessoas ao redor do mundo, de acordo com a ONU, o mundo tinha cerca de 733 milhões de pessoas passando fome em 2023, ou 1 em cada 11 pessoas.</p>
+                        <p>É fato que não podemos zerar esses números sozinhos, precisamos de corações solidarios, que se importam com a dor do outro e acima de tudo um entendimento de que temos que nos unir em prol disso, são pessoas, são crianças, são vidas... </p>
+                        <p>A FoodBridge é um site aonde conectamos pessoas que estão dispostas a ajudar em doações a pessoas que estão disposta a ajudar distribuindo os alimentos.</p>
+                        <div className="home-page-more-details-description-phrases">
+                            <h3>Unidos por pessoas</h3>
+                            <div className="home-page-more-details-column">
+                            </div>
+                            <h3>Unidos contra a fome</h3>
+                        </div>
+                        <a href="">Doe agora <img src={arrowIconWhite} alt="" /></a>
                     </div>
                 </div>
             </section>
