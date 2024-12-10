d75603156a8e31e12bff995d6fae289493226a8e
EduGollner0609 <dudugollner@gmail.com>
2024-12-10 15:18:26 -0300

    Criando card para registrar doação e estilizando.

--- frontend/src/routes/CommunityPage/DonationRegisterPage/index.tsx
+++ frontend/src/routes/CommunityPage/DonationRegisterPage/index.tsx
@@ -6,7 +6,22 @@ export default function DonationRegisterPage() {
             <section id="section-donation-register">
                 <div className="donation-register-content container">
                     <div className="card-donation-register">
+                        <h2>Doação</h2>
+                        <form >
+                            <div className="card-donation-register-item">
+                                <label>Nome</label>
+                                <input type="text" />
+                            </div>
+                            <div className="card-donation-register-item">
+                                <label>Nome</label>
+                                <input type="text" />
+                            </div>
+                            <div className="card-donation-register-item">
+                                <label>Nome</label>
+                                <textarea></textarea>
+                            </div>
 
+                        </form>
                     </div>
                 </div>
             </section>
