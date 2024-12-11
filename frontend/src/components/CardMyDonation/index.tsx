f0f02b86cf2492025454278f773ce409b3f889aa
EduGollner0609 <dudugollner@gmail.com>
2024-12-11 17:16:48 -0300

    Criando card my donation.

---
+++ frontend/src/components/CardMyDonation/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+export default function CardMyDonation() {
+
+
+    return (
+        <div className="card-my-donation">
+            <div className="card-donation-data">
+                <h3>Eduardo Sousa Gollner</h3>
+                <p>Cesta básica mais uma banana</p>
+            </div>
+            <p>Vitória, Espirito Santo</p>
+        </div>
+    );
+}
--- frontend/src/routes/CommunityPage/DonationsPage/index.tsx
+++ frontend/src/routes/CommunityPage/DonationsPage/index.tsx
@@ -1,10 +1,32 @@
+import { Link } from 'react-router-dom';
 import './styles.css'
+import CardDonation from '../../../components/CardDonation';
+import CardMyDonation from '../../../components/CardMyDonation';
 
 export default function DonationsPage() {
     return (
         <main>
             <section id="section-donations">
-                
+                <div className="donations-content container">
+                    <h2>Suas <span>Doações</span></h2>
+                    <div className="my-donations-space">
+                        <div className="donations-plus-btn">
+                            <Link to="/community/donation-register">
+                                Fazer Doação
+                            </Link>
+                        </div>
+                        <div className="card-my-donations-list">
+                            <CardMyDonation />
+                            <CardMyDonation />
+                            <CardMyDonation />
+                            <CardMyDonation />
+                            <CardMyDonation />
+                            <CardMyDonation />
+                            <CardMyDonation />
+
+                        </div>
+                    </div>
+                </div>
             </section>
         </main>
     );
