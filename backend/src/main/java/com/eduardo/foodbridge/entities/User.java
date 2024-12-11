6c15ff9d5e693f51182e1376a5da61e1dff9a943
EduGollner0609 <dudugollner@gmail.com>
2024-12-11 17:56:21 -0300

    Criando entidade Role.!

--- backend/src/main/java/com/eduardo/foodbridge/entities/User.java
+++ backend/src/main/java/com/eduardo/foodbridge/entities/User.java
@@ -1,14 +1,20 @@
 package com.eduardo.foodbridge.entities;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private LocalDate birthDate;
@@ -74,6 +80,23 @@ public class User {
 
 	public void setPassowrd(String passowrd) {
 		this.passowrd = passowrd;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
