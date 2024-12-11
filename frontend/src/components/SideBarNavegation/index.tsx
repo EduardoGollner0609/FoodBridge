7370052f385bf7df4a73648f69481338b65e99c9
EduGollner0609 <dudugollner@gmail.com>
2024-12-11 13:05:47 -0300

    Adicionando links e estilo especial quando ativo.

--- frontend/src/components/SideBarNavegation/index.tsx
+++ frontend/src/components/SideBarNavegation/index.tsx
@@ -3,6 +3,7 @@ import homeIcon from '../../assets/home-icon.svg';
 import userIcon from '../../assets/user-icon-navegation.svg';
 import donationFoodIcon from '../../assets/donation-food-icon.svg';
 import exitIcon from '../../assets/exit-icon.svg';
+import { NavLink } from 'react-router-dom';
 
 export default function SideBarNavegation() {
     return (
@@ -13,35 +14,43 @@ export default function SideBarNavegation() {
 
             <ul className="side-bar-navegation-options">
                 <li className="side-bar-option-item">
-                    <a href="">
+                    <NavLink to="/community/home" className={({ isActive }) =>
+                        isActive ? "navlink-active-community" : ""
+                    }>
                         <span className="side-bar-option-item-icon">
                             <img src={homeIcon} alt="" />
                         </span>
                         <span className="side-bar-option-item-text">
                             Home
                         </span>
-                    </a>
+                    </NavLink>
 
                 </li>
                 <li className="side-bar-option-item">
-                    <a href="">
+                    <NavLink to="/community/user-details" className={({ isActive }) =>
+                        isActive ? "navlink-active-community" : ""
+                    }>
                         <span className="side-bar-option-item-icon">
                             <img src={userIcon} alt="" />
                         </span>
                         <span className="side-bar-option-item-text">
                             Ver Perfil
                         </span>
-                    </a>
+
+                    </NavLink>
+
                 </li>
                 <li className="side-bar-option-item">
-                    <a href="">
+                    <NavLink to="/community/donation-register" className={({ isActive }) =>
+                        isActive ? "navlink-active-community" : ""
+                    }>
                         <span className="side-bar-option-item-icon">
                             <img src={donationFoodIcon} alt="" />
                         </span>
                         <span className="side-bar-option-item-text">
                             Doar
                         </span>
-                    </a>
+                    </NavLink>
 
                 </li>
                 <li className="side-bar-option-item">
