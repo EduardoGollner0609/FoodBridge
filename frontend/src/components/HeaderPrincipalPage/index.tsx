f936bb3944e18c76557101f2bf0841ef9cc08b27
EduGollner0609 <dudugollner@gmail.com>
2024-12-11 13:07:43 -0300

    Adicionando Link ao titulo no header para voltar para a pagina de doações.

--- frontend/src/components/HeaderPrincipalPage/index.tsx
+++ frontend/src/components/HeaderPrincipalPage/index.tsx
@@ -1,12 +1,15 @@
 import './styles.css';
 import userIcon from '../../assets/user-icon.svg';
+import { Link } from 'react-router-dom';
 
 export default function HeaderPrincipalPage() {
     return (
         <header>
             <nav className="container">
                 <div className="fb-header-community-page-title">
-                    <h1>FoodBridge</h1>
+                    <Link to="/community">
+                        <h1>FoodBridge</h1>
+                    </Link>
                 </div>
                 <div className="fb-user-informations-community-page">
                     <a href="">
