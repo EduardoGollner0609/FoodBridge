f5ccdef15350c8fb16a991b452069a444261ac96
EduGollner0609 <dudugollner@gmail.com>
2024-12-11 13:51:31 -0300

    Criando menu mobile.

--- frontend/src/components/HeaderHomePage/index.tsx
+++ frontend/src/components/HeaderHomePage/index.tsx
@@ -1,15 +1,18 @@
 import { NavLink } from 'react-router-dom';
 import './styles.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function HeaderHomePage() {
+
+    const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
     return (
         <header>
             <nav className="container">
-                <div className="fb-header-introduction-page-title">
+                <div className="header-introduction-page-title">
                     <h1>FoodBridge</h1>
                 </div>
-                <div className="fb-header-introduction-page-navegation-options">
+                <div className="header-introduction-page-navegation-options">
                     <ul>
                         <NavLink to="/home">
                             <li>Inicio</li>
@@ -22,11 +25,45 @@ export default function HeaderHomePage() {
                         </NavLink>
                     </ul>
                 </div>
-                <div className="fb-header-introduction-page-login">
+                <div className="header-introduction-page-login">
                     <Link to="/login">
                         Entrar
                     </Link>
                 </div>
+                <div className="header-introduction-page-mobile-menu">
+                    ☰
+                </div>
+                {
+                    mobileMenuVisible &&
+                    <>
+                        <div className="options-navegation-mobile">
+                            <div className="close-mobile-menu-icon">
+                                X
+                            </div>
+                            <ul>
+                                <li>
+                                    <NavLink to="/">
+                                        Inicio
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/">
+                                        Sobre
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/">
+                                        Fale Conosco
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="overlay-menu">
+
+                        </div>
+                    </>
+
+                }
             </nav>
         </header>
     );
