f686430533a1526267189c403ab72e8c07668b22
EduGollner0609 <dudugollner@gmail.com>
2024-12-11 18:01:31 -0300

    Criando tabela fruto da relação de User e Role.

--- backend/src/main/java/com/eduardo/foodbridge/entities/User.java
+++ backend/src/main/java/com/eduardo/foodbridge/entities/User.java
@@ -1,12 +1,17 @@
 package com.eduardo.foodbridge.entities;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +26,10 @@ public class User {
 	private String phone;
 	private String email;
 	private String passowrd;
+
+	@ManyToMany
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 
 	public User() {
 	}
@@ -80,6 +89,23 @@ public class User {
 
 	public void setPassowrd(String passowrd) {
 		this.passowrd = passowrd;
+	}
+
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
