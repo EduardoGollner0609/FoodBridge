0eca5e79095a7079071571759832bf9088d97c74
EduGollner0609 <dudugollner@gmail.com>
2024-12-12 00:43:06 -0300

    Criando entidade Donation e relacionamento com User.

---
+++ backend/src/main/java/com/eduardo/foodbridge/entities/Donation.java
@@ -0,0 +1,57 @@
+package com.eduardo.foodbridge.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_donation")
+public class Donation {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String description;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public Donation() {
+	}
+
+	public Donation(Long id, String description, User user) {
+		this.id = id;
+		this.description = description;
+		this.user = user;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
--- backend/src/main/java/com/eduardo/foodbridge/entities/User.java
+++ backend/src/main/java/com/eduardo/foodbridge/entities/User.java
@@ -1,8 +1,10 @@
 package com.eduardo.foodbridge.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -16,6 +18,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -37,6 +40,9 @@ public class User implements UserDetails {
 	@ManyToMany
 	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
+	
+	@OneToMany(mappedBy ="user")
+	private List<Donation> donations = new ArrayList<>();
 
 	public User() {
 	}
