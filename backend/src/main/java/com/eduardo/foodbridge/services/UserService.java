34d41a9c3a488b15e3de2a4ee4a5c6669a48b08c
EduGollner0609 <dudugollner@gmail.com>
2024-12-11 23:47:25 -0300

    Criando endpoint para criar usuario.

--- backend/src/main/java/com/eduardo/foodbridge/services/UserService.java
+++ backend/src/main/java/com/eduardo/foodbridge/services/UserService.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.eduardo.foodbridge.dtos.UserDTO;
+import com.eduardo.foodbridge.entities.Role;
 import com.eduardo.foodbridge.entities.User;
 import com.eduardo.foodbridge.repositories.UserRepository;
 
@@ -16,6 +18,22 @@ public class UserService implements UserDetailsService {
 
 	@Autowired
 	private UserRepository repository;
+
+	public UserDTO insert(UserDTO userDTO) {
+		User user = new User();
+		copyDtoToEntity(user, userDTO);
+		return new UserDTO(repository.save(user));
+	}
+
+	private void copyDtoToEntity(User user, UserDTO userDTO) {
+		user.setEmail(userDTO.getEmail());
+		user.setName(userDTO.getName());
+		user.setPassowrd(userDTO.getPassword());
+		user.setBirthDate(userDTO.getBirthDate());
+		user.setPhone(userDTO.getPhone());
+
+		user.addRole(new Role(1L, "ROLE_USER"));
+	}
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
